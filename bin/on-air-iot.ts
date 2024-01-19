5856703fb290bcd682e9da51c1875144e76273c7
azy2k9 <azy2k9@gmail.com>
2024-01-19 12:02:45 +0000

    Restructure files

--- bin/on-air-iot.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { OnAirIotStack } from '../lib/on-air-iot-stack';
-
-const app = new cdk.App();
-new OnAirIotStack(app, 'OnAirIotStack');
--- test/on-air-iot.test.ts
+++ test/on-air-iot.test.ts
@@ -1,17 +1,17 @@
-import * as cdk from 'aws-cdk-lib';
-import { Template, Match } from 'aws-cdk-lib/assertions';
-import * as OnAirIot from '../lib/on-air-iot-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template, Match } from "aws-cdk-lib/assertions";
+import * as OnAirIot from "../cdk/stack";
 
-test('SQS Queue and SNS Topic Created', () => {
+test("SQS Queue and SNS Topic Created", () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new OnAirIot.OnAirIotStack(app, 'MyTestStack');
+  const stack = new OnAirIot.OnAirIotStack(app, "MyTestStack");
   // THEN
 
   const template = Template.fromStack(stack);
 
-  template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
+  template.hasResourceProperties("AWS::SQS::Queue", {
+    VisibilityTimeout: 300,
   });
-  template.resourceCountIs('AWS::SNS::Topic', 1);
+  template.resourceCountIs("AWS::SNS::Topic", 1);
 });
